ce41b4c307e1a44205dbd2ce240286bf83770e8e
Jack Lynch <jacklynchtds@gmail.com>
2016-02-14 00:44:15 -0500

    updated Base for IMU; begun RNCBH

---
+++ RNCBH.java
@@ -0,0 +1,36 @@
+
+
+public class RC_FromBase extends Base5459 {
+
+    public RNCBH() { }
+
+    int v_state = 0;
+    @Override
+    public void loop() {
+        switch (v_state) {
+            case 0:
+                wire.setPosition(0.5);
+                v_state++;
+                try { Thread.sleep(1000); }
+                catch(Exception ex) { }
+                break;
+            case 1:
+                ziplineLeft.setPosition(1);
+                try { Thread.sleep(1000); }
+                catch(Exception ex) { }
+                v_state++;
+                break;
+
+            default:
+                stop(); // [[TODO: test this to see if it works properly]]
+        }
+    }
+
+    @Override
+    public void stop() {
+        drive_left_front.setPower(0);
+        drive_left_back.setPower(0);
+        drive_right_front.setPower(0);
+        drive_right_back.setPower(0);
+    }
+}
