dfad1f8790f3b170b677222d4bc96662b227ceb9
Trinity School Robotics <trinityrobotics@tsdch.org>
2016-02-19 16:56:53 -0500

    working delay chooser code

--- DelayChoose.java
+++ DelayChoose.java
@@ -23,32 +23,34 @@ public class DelayChoose extends OpMode {
     @Override
     public void loop() {
         // driver display
-        String timeDisplay = String.format("Time: %d", waitTime);
-        telemetry.addData("01", timeDisplay);
-        if(!hasChosen) { telemetry.addData("02", "Choosing..."); }
-        else { telemetry.addData("02", "Delay chosen! Change OpMode now."); }
+        telemetry.addData("Seconds to delay", waitTime);
+        if(!hasChosen) { telemetry.addData("", "Choosing..."); }
+        else { telemetry.addData("", "Delay chosen! Change OpMode now."); }
 
-        // read the gamepads. if any key is press, add a small debounce delay
-        if(gamepad1.dpad_up || gamepad1.dpad_down || gamepad1.a) {
-            if(gamepad1.dpad_up) {
-                waitTime++;
-            }
-            else if(gamepad1.dpad_down) {
-                waitTime = waitTime > 0 ? waitTime-- : waitTime; // clip the time at 0
-            }
-            else if(gamepad1.a) {
-                try {
-                    hasChosen = true;
-                    PrintWriter pw = new PrintWriter(timeDelay);
-                    pw.println(waitTime);
-                    pw.close();
-                }
-                catch(Exception e) {
-                    telemetry.addData("03", "Unable to write out to file!");
-                }
-            }
-            try { Thread.sleep(300); }
-            catch(Exception e) { }
+        // read the gamepads. DPad must be released between each press.
+        if(gamepad1.dpad_up && !dpadUp) {
+            waitTime++;
         }
+
+        if(gamepad1.dpad_down && !dpadDown) {
+            waitTime--;
+        }
+
+        if(gamepad1.a) {
+            waitTime = waitTime >= 0 ? waitTime : 0; // clip at 0
+            try {
+                hasChosen = true;
+                PrintWriter pw = new PrintWriter(timeDelay);
+                pw.println(waitTime);
+                pw.close();
+            }
+            catch(Exception e) {
+                telemetry.addData("Error!", "Unable to write out to file.");
+            }
+        }
+
+        // set the debouncing variables to the current gamepad state
+        dpadUp = gamepad1.dpad_up;
+        dpadDown = gamepad1.dpad_down;
     }
 }
