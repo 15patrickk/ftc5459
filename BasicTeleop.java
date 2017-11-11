f9eb6cb0b3c2c8351fcdafce99361878d65ec2e2
Trinity Robotics <5459desktop@robotics.tsdch>
2017-11-11 11:07:54 -0900

    updates to 11 November

--- BasicTeleop.java
+++ BasicTeleop.java
@@ -32,17 +32,43 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 package org.firstinspires.ftc.teamcode;
 
+import android.graphics.Color;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.RobotLog;
 
-@TeleOp(name="5459 Teleop", group="WoodyBot")
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.MatrixF;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@TeleOp(name="potato op", group="WoodyBot")
 public class BasicTeleop extends OpMode {
 
     /* Declare OpMode members. */
-    WoodyBot         robot   = new WoodyBot();   // Use robot's hardware
-    private ElapsedTime     runtime = new ElapsedTime();
+    WoodyBot robot = new WoodyBot();   // Use robot's hardware
+    private ElapsedTime runtime = new ElapsedTime();
 
+    double Rposition = 0.5;
+    double Lposition = 0.5;
     // code runs ONCE when driver hits INIT
     @Override
     public void init() {
@@ -54,24 +80,39 @@ public class BasicTeleop extends OpMode {
         // Send telemetry message to signify robot waiting;
         telemetry.addData("Say", "I like potatos");    //
         updateTelemetry(telemetry);
+        if (robot.CS instanceof SwitchableLight) {
+            ((SwitchableLight)robot.CS).enableLight(true);
+        }
     }
 
     // code runs REPEATEDLY after the driver hits INIT, but before they hit PLAY
     @Override
-    public void init_loop() { }
+    public void init_loop() {
+    }
 
     // code runs ONCE when the driver hits PLAY
     @Override
-    public void start() { }
+    public void start() {
+    }
 
     // code runs REPEATEDLY when driver hits play
     // put driver controls (drive motors, servos, etc.) HERE
     @Override
     public void loop() {
-        double ThrottleUp = gamepad1.left_trigger;
-        boolean Throttledown = gamepad1.left_bumper;
+        //int color = robot.CS.red();
+        double elevup = gamepad2.right_trigger;
+        boolean elevdown = gamepad2.right_bumper;
         double ThrottleLeft = gamepad1.left_stick_y;
         double ThrottleRight = gamepad1.right_stick_y;
+        boolean dooropen = gamepad2.b;
+        boolean doorclose = gamepad2.a;
+        double extendin = gamepad2.left_trigger;
+        boolean extendout = gamepad2.left_bumper;
+        boolean rotateup = gamepad2.y;
+        boolean rotatedown = gamepad2.x;
+        boolean clawclose = gamepad2.dpad_up;
+        boolean clawopen = gamepad2.dpad_down;
+
 
         // this is technically the correct way to do this. -PK
 //        double ElevatorPower = 0;
@@ -88,25 +129,87 @@ public class BasicTeleop extends OpMode {
 //            telemetry.addData("Elevator","OFF");
 //        }
 
-        if(Throttledown == true)
-        {
-            robot.Elevator.setPower(-1);
-            telemetry.addData("elevator","on");
+        if (rotateup == true) {
+            robot.rotator.setPosition(.7);
         }
-        else if(Throttledown == false)
-        {
-            robot.Elevator.setPower(0);
-            telemetry.addData("elevator","off");
+        if (rotatedown == true) {
+            robot.rotator.setPosition(0.0);
         }
-        robot.Elevator.setPower(ThrottleUp);
-        robot.FrontMotorLeft.setPower(ThrottleLeft);
-        robot.FrontMotorRight.setPower(-ThrottleRight);
+        if (clawopen) {
+            robot.claw.setPosition(1);
+        }
+        if (clawclose) {
+            robot.claw.setPosition(0.0);
+        }
+        if (extendout) {
+            robot.Extender.setPower(.5);
+        } else if (extendout == false) {
+            robot.Extender.setPower(0);
+        }
+        if (dooropen) {
+            robot.left_door.setPosition(.5);
+            robot.right_door.setPosition(0.0);
+            telemetry.addData("door", "open");
+        }
+        if (doorclose) {
+            robot.left_door.setPosition(.9);
+            robot.right_door.setPosition(0.3);
+            telemetry.addData("door", "closed");
 
+
+        }
+        if (elevup > .01) {
+            robot.Elevator1.setPower(-1);
+            telemetry.addData("elevator", "up");
+        } else if (elevup == 0) {
+            robot.Elevator1.setPower(0.0);
+            telemetry.addData("elevator", " stopped");
+        }
+        if (elevdown == true) {
+            robot.Elevator1.setPower(1.0);
+            telemetry.addData("elevator", " down");
+
+        } else if (elevdown == false) {
+            robot.Elevator1.setPower(0.0);
+            telemetry.addData("elevator", " stopped");
+        }
+
+        if (Rposition > 1) {
+            Rposition = 1;
+        } else if (Rposition < 0) {
+            Rposition = 0;
+        }
+        if (Lposition > 1) {
+            Lposition = 1;
+        } else if (Rposition < 0) {
+            Rposition = 0;
+        }
+
+
+        robot.FrontMotorLeft.setPower(ThrottleLeft / 1.5);
+        robot.FrontMotorRight.setPower(-ThrottleRight / 1.5);
+        //color = robot.CS.red();
         // telemetry
+        //telemetry.addData("Red",color);
         telemetry.addData("left", "%.2f", ThrottleLeft);
         telemetry.addData("right", "%.2f", ThrottleRight);
-        telemetry.addData("up",".%.2f",ThrottleUp);
+        NormalizedRGBA colors;
+        colors = robot.CS.getNormalizedColors();
+
+        /** Use telemetry to display feedback on the driver station. We show the conversion
+         * of the colors to hue, saturation and value, and display the the normalized values
+         * as returned from the sensor.
+         * @see <a href="http://infohost.nmt.edu/tcc/help/pubs/colortheory/web/hsv.html">HSV</a>*/
+
+
+        telemetry.addLine()
+                .addData("a", "%.3f", colors.alpha)
+                .addData("r", "%.3f", colors.red)
+                .addData("g", "%.3f", colors.green)
+                .addData("b", "%.3f", colors.blue);
+
 
         updateTelemetry(telemetry);
     }
 }
+
--- WoodyBot.java
+++ WoodyBot.java
@@ -8,11 +8,15 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoController;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.vuforia.ar.pl.SensorController;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
 
 /**
  * This is NOT an opmode.
@@ -20,57 +24,75 @@ import com.vuforia.ar.pl.SensorController;
  *
  */
 public class WoodyBot {
-    // define all hardware on robot
+ // define all hardware on robot
 
-    public DcMotor FrontMotorLeft;
-    public DcMotor FrontMotorRight;
-    public DcMotor Elevator;
+ public DcMotor FrontMotorLeft;
+ public DcMotor FrontMotorRight;
+ public DcMotor Elevator1;
+ public DcMotor Extender;
+ public Servo right_door;
+ public Servo left_door;
+ public Servo claw;
+ public Servo rotator;
+ public NormalizedColorSensor CS;
 
-    /* local OpMode members. */
-    HardwareMap hwMap           =  null;
-    private ElapsedTime period  = new ElapsedTime();
 
-    public WoodyBot(){ }
+ /* local OpMode members. */
 
-    /* Initialize standard Hardware interfaces */
-    public void init(HardwareMap ahwMap) {
-        hwMap = ahwMap; // reference to hardware map
+ HardwareMap hwMap           =  null;
+ private ElapsedTime period  = new ElapsedTime();
 
-        // initialize motors
-        Elevator = hwMap.dcMotor.get("Elevator");
-        Elevator.setDirection(DcMotor.Direction.REVERSE);
-        FrontMotorLeft = hwMap.dcMotor.get("FrontMotorLeft");
-        FrontMotorLeft.setDirection(DcMotor.Direction.REVERSE);
-        FrontMotorRight = hwMap.dcMotor.get("FrontMotorRight");
-        FrontMotorLeft.setDirection(DcMotor.Direction.REVERSE);
+ public WoodyBot(){ }
 
-        // initialize servos
+ /* Initialize standard Hardware interfaces */
 
-         // Set all motors and servos to zero power
-        FrontMotorLeft.setPower(0.0);
-        FrontMotorRight.setPower(0.0);
-        Elevator.setPower(0.0);
-    }
+ public void init(HardwareMap ahwMap) {
+ hwMap = ahwMap; // reference to hardware map
 
-    /***
-     *
-     * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
-     * periodic tick.  This is used to compensate for varying processing times for each cycle.
-     * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
-     *
-     * @param periodMs  Length of wait cycle in mSec.
-     * @throws InterruptedException
-     */
-    public void waitForTick(long periodMs) throws InterruptedException {
+ // initialize motors
+ Extender = hwMap.dcMotor.get("Extend");
+ Elevator1 = hwMap.dcMotor.get("Elevator1");
+ FrontMotorLeft = hwMap.dcMotor.get("FrontMotorLeft");
+ FrontMotorRight = hwMap.dcMotor.get("FrontMotorRight");
+ //CS = hwMap.colorSensor.get("CS");
+ right_door = hwMap.servo.get("right_door");
+ left_door = hwMap.servo.get("left_door");
+ claw = hwMap.servo.get("claw");
+ rotator = hwMap.servo.get("rot");
+  CS = hwMap.get(NormalizedColorSensor.class, "CS");
+ // initialize servos
 
-        long  remaining = periodMs - (long)period.milliseconds();
+ // Set all motors and servos to zero power
+ FrontMotorLeft.setPower(0.0);
+ FrontMotorRight.setPower(0.0);
+ Elevator1.setPower(0.0);
+ //right_door.setPosition(0.0);   //Enabling these caused the servos to move in init, which is illegal
+ //left_door.setPosition(0.0);
+ claw.setPosition(0.0);
+ rotator.setPosition(0.5);
+// CS.enableLed(false);
 
-        // sleep for the remaining portion of the regular cycle period.
-        if (remaining > 0)
-            Thread.sleep(remaining);
+ }
 
-        // Reset the cycle clock for the next pass.
-        period.reset();
-    }
-}
 
+ //*
+ //* waitForTick implements a periodic delay. However, this acts like a metronome with a regular
+ //* periodic tick.  This is used to compensate for varying processing times for each cycle.
+ //* The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
+ //*
+ //* @param periodMs  Length of wait cycle in mSec.
+ //* @throws InterruptedException
+
+
+ public void waitForTick(long periodMs) throws InterruptedException {
+
+ long  remaining = periodMs - (long)period.milliseconds();
+
+ // sleep for the remaining portion of the regular cycle period.
+ if (remaining > 0)
+ Thread.sleep(remaining);
+
+ // Reset the cycle clock for the next pass.
+ period.reset();
+ }
+ }
